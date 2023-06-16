ad3404d84550ac6783b3dc3e5752a9f0432b6255
Bogatyreva Natalya <124141494+Tanshi0@users.noreply.github.com>
2023-06-16 08:24:47 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+import NotesAPI from "./NotesApi.js"
+import NotesUI from "./NotesUI.js"
+
+
+window.addEventListener("load", main);
+
+function main() {
+
+
+  const themeBtn = document.getElementById("theme-btn");
+  const iconTheme = document.getElementById("theme-btn");
+  const page = document.getElementById("page");
+  const icon = document.getElementById("theme-btn");
+  const sidebar = document.getElementById("sidebar-wrapper");
+  const menuBtn = document.getElementById("menu-btn");
+  const subMenuBtn = document.getElementById("main-menu-additional-btn");
+  const subMenuBtnIcon = document.getElementById("main-menu-additional-btn-icon");
+  const subMenu = document.getElementById("main-menu-additional");
+  const notesPreview = document.getElementById("notes-preview");
+
+
+  const notesData = new NotesAPI();
+  const app = new NotesUI(notesData, page);
+
+  themeBtn.addEventListener("click", () => {
+    page.classList.toggle('page_dark');
+  });
+
+
+  iconTheme.addEventListener("click", () => {
+    icon.classList.toggle('icon_theme');
+  });
+
+  menuBtn.addEventListener("click", () => {
+    sidebar.classList.toggle('sidebar-wrapper_hidden');
+    menuBtn.classList.toggle('button_active');
+  });
+
+  subMenuBtn.addEventListener("click", () => {
+    subMenu.classList.toggle("main-menu-additional_visible");
+    subMenuBtn.classList.toggle("button_active");
+    subMenuBtnIcon.classList.toggle("main-menu-additional-btn-icon_active");
+  });
+
+}
